b46c41ed60504a5dbb3d0db7cc338dd75bab6425
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-04-10 05:33:47 -0400

    Can access stored procedures now

--- stockup-api/common/models/tbl-tickets.js
+++ stockup-api/common/models/tbl-tickets.js
@@ -1,3 +1,50 @@
 "use strict";
 
-module.exports = function (Tbltickets) {};
+var DataSource = require("loopback-datasource-juggler").DataSource;
+var dataSource = new DataSource({
+  connector: require("loopback-connector-mysql"),
+  host: "db431.cjeog1zo7yyf.us-east-2.rds.amazonaws.com",
+  port: 3306,
+  database: "SIS",
+  user: "master431",
+  password: "masterhot1",
+});
+module.exports = function (Tbltickets) {
+  Tbltickets.getEndDayPrevDayTickets = function (cb) {
+    dataSource.connector.execute("CALL `getEndDayPrevDayTickets`();", function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("getEndDayPrevDayTickets", {
+    accepts: {},
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/getEndDayPrevDayTickets", verb: "get" },
+  });
+
+  Tbltickets.getGame = function (game, cb) {
+    dataSource.connector.execute("CALL `getGame`(" + game + ");", function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("getGame", {
+    accepts: { arg: "game", type: "string", required: "true" },
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/getGame", verb: "get" },
+  });
+};
