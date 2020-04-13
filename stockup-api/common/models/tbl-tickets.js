3febb66ebf9696807caf1f367535b620e91cf7c9
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-04-13 16:11:26 -0400

    Added more stored procedures

--- stockup-api/common/models/tbl-tickets.js
+++ stockup-api/common/models/tbl-tickets.js
@@ -9,6 +9,7 @@ var dataSource = new DataSource({
   user: "master431",
   password: "masterhot1",
 });
+
 module.exports = function (Tbltickets) {
   // ACTIVATE PACK
   Tbltickets.activateTicket = function (Game, Pack, Nbr, Emp_id, cb) {
@@ -59,8 +60,8 @@ module.exports = function (Tbltickets) {
   });
 
   // GET END DAY PREVIOUS DAY TICKETS
-  Tbltickets.getEndDayPrevDayTickets = function (cb) {
-    dataSource.connector.execute("CALL `getEndDayPrevDayTickets`();", function (
+  Tbltickets.endDayPrevDayTickets = function (cb) {
+    dataSource.connector.execute("CALL `endDayPrevDayTickets`();", function (
       err,
       data
     ) {
@@ -72,13 +73,13 @@ module.exports = function (Tbltickets) {
     });
   };
 
-  Tbltickets.remoteMethod("getEndDayPrevDayTickets", {
+  Tbltickets.remoteMethod("endDayPrevDayTickets", {
     returns: { arg: "result", type: "any", root: "true" },
-    http: { path: "/getEndDayPrevDayTickets", verb: "get" },
+    http: { path: "/endDayPrevDayTickets", verb: "get" },
   });
 
   // GET GAME
-  Tbltickets.getGame = function (game, cb) {
+  Tbltickets.game = function (game, cb) {
     dataSource.connector.execute("CALL `getGame`(" + game + ");", function (
       err,
       data
@@ -91,34 +92,14 @@ module.exports = function (Tbltickets) {
     });
   };
 
-  Tbltickets.remoteMethod("getGame", {
+  Tbltickets.remoteMethod("game", {
     accepts: { arg: "game", type: "string", required: "true" },
     returns: { arg: "result", type: "any", root: "true" },
-    http: { path: "/getGame", verb: "get" },
-  });
-
-  // GET DAILY COUNTS PER GAME
-  Tbltickets.getDailyCounts = function (cb) {
-    dataSource.connector.execute("CALL `dailyCounts`();", function (err, data) {
-      if (err) {
-        console.log("Error:", err);
-      }
-      console.log("datum:", data);
-      cb(null, data);
-    });
-  };
-
-  Tbltickets.remoteMethod("getDailyCounts", {
-    returns: {
-      arg: "result",
-      type: [{ SUM: "any", Game: "any" }],
-      root: "true",
-    },
-    http: { path: "/getDailyCounts", verb: "get" },
+    http: { path: "/game", verb: "get" },
   });
 
   // DELETE PACK
-  Tbltickets.deletePack = function (Game, Pack, cb) {
+  Tbltickets.pack = function (Game, Pack, cb) {
     var sql = "CALL `deleteGamePack`(" + Game + "," + Pack + ");";
     dataSource.connector.execute(sql, function (err, data) {
       if (err) {
@@ -129,7 +110,7 @@ module.exports = function (Tbltickets) {
     });
   };
 
-  Tbltickets.remoteMethod("deletePack", {
+  Tbltickets.remoteMethod("pack", {
     accepts: [
       {
         arg: "Game",
@@ -143,11 +124,11 @@ module.exports = function (Tbltickets) {
       },
     ],
     returns: { arg: "result", type: "any", root: "true" },
-    http: { path: "/deletePack", verb: "delete" },
+    http: { path: "/pack", verb: "delete" },
   });
 
   // SET END DAY TICKET
-  Tbltickets.setEndDayTicket = function (Game, Pack, Nbr, Emp_id, cb) {
+  Tbltickets.endDayTicket = function (Game, Pack, Nbr, Emp_id, cb) {
     var sql =
       "CALL `endDayEmp`(" +
       Game +
@@ -167,7 +148,7 @@ module.exports = function (Tbltickets) {
     });
   };
 
-  Tbltickets.remoteMethod("setEndDayTicket", {
+  Tbltickets.remoteMethod("endDayTicket", {
     accepts: [
       {
         arg: "Game",
@@ -191,11 +172,11 @@ module.exports = function (Tbltickets) {
       },
     ],
     returns: { arg: "result", type: "any", root: "true" },
-    http: { path: "/setEndDayTicket", verb: "patch" },
+    http: { path: "/endDayTicket", verb: "patch" },
   });
 
   // SET START DAY TICKET
-  Tbltickets.setStartDayTicket = function (Game, Pack, Nbr, Emp_id, cb) {
+  Tbltickets.startDayTicket = function (Game, Pack, Nbr, Emp_id, cb) {
     var sql =
       "CALL `startDayEmp`(" +
       Game +
@@ -215,7 +196,7 @@ module.exports = function (Tbltickets) {
     });
   };
 
-  Tbltickets.remoteMethod("setStartDayTicket", {
+  Tbltickets.remoteMethod("startDayTicket", {
     accepts: [
       {
         arg: "Game",
@@ -239,6 +220,262 @@ module.exports = function (Tbltickets) {
       },
     ],
     returns: { arg: "result", type: "any", root: "true" },
-    http: { path: "/setStartDayTicket", verb: "patch" },
+    http: { path: "/startDayTicket", verb: "patch" },
+  });
+
+  // GET SPARE PACKS BY GAME NAME
+  Tbltickets.sparePacks = function (game, cb) {
+    dataSource.connector.execute(
+      "CALL `getSparePacksByGame`(" + game + ");",
+      function (err, data) {
+        if (err) {
+          console.log("Error:", err);
+        }
+        console.log("datum:", data);
+        cb(null, data);
+      }
+    );
+  };
+
+  Tbltickets.remoteMethod("sparePacks", {
+    accepts: { arg: "game", type: "string", required: "true" },
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/sparePacks", verb: "get" },
+  });
+
+  // GET GAME PACK NBR RETURN EVERYTHING
+  Tbltickets.groupTicket = function (Game, Pack, Nbr, cb) {
+    var sql =
+      "CALL `getGamePackNbrReturnEverything`(" +
+      Game +
+      "," +
+      Pack +
+      ", " +
+      Nbr +
+      ");";
+    dataSource.connector.execute(sql, function (err, data) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("groupTicket", {
+    accepts: [
+      {
+        arg: "Game",
+        type: "string",
+        required: "true",
+      },
+      {
+        arg: "Pack",
+        type: "string",
+        required: "true",
+      },
+      {
+        arg: "Nbr",
+        type: "string",
+        required: "true",
+      },
+    ],
+    returns: {
+      arg: "result",
+      type: "any",
+      root: "true",
+    },
+    http: { path: "/groupTicket", verb: "get" },
+  });
+
+  // GET MONTHLY COUNTS
+  Tbltickets.monthlyCounts = function (cb) {
+    dataSource.connector.execute("CALL `monthlyCounts`();", function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("monthlyCounts", {
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/monthlyCounts", verb: "get" },
+  });
+
+  // WEEKLY COUNTS
+  Tbltickets.weeklyCounts = function (cb) {
+    dataSource.connector.execute("CALL `weeklyCounts`();", function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("weeklyCounts", {
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/weeklyCounts", verb: "get" },
+  });
+
+  // GET DAILY COUNTS PER GAME
+  Tbltickets.dailyCounts = function (cb) {
+    dataSource.connector.execute("CALL `dailyCounts`();", function (err, data) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("dailyCounts", {
+    returns: {
+      arg: "result",
+      type: [{ SUM: "any", Game: "any" }],
+      root: "true",
+    },
+    http: { path: "/dailyCounts", verb: "get" },
+  });
+
+  // SEED NEW GAME
+  Tbltickets.seedNewGame = function (Game, Pack, Nbr, Name, Price, Emp_id, cb) {
+    var sql =
+      "CALL `seedNewGame`(" +
+      Game +
+      "," +
+      Pack +
+      "," +
+      Nbr +
+      ",'" +
+      Name +
+      "'," +
+      Price +
+      ",'" +
+      Emp_id +
+      "');";
+    dataSource.connector.execute(sql, function (err, data) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("seedNewGame", {
+    accepts: [
+      {
+        arg: "Game",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Pack",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Nbr",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Name",
+        type: "string",
+        required: "true",
+      },
+      {
+        arg: "Price",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Emp_id",
+        type: "string",
+        required: "true",
+      },
+    ],
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/seedNewGame", verb: "post" },
+  });
+
+  // SEED EXISTING GAME
+  Tbltickets.seedExistingGame = function (Game, Pack, Nbr, Emp_id, cb) {
+    var sql =
+      "CALL `seedExistingGame`(" +
+      Game +
+      "," +
+      Pack +
+      "," +
+      Nbr +
+      ",'" +
+      Emp_id +
+      "');";
+    dataSource.connector.execute(sql, function (err, data) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("seedExistingGame", {
+    accepts: [
+      {
+        arg: "Game",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Pack",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Nbr",
+        type: "number",
+        required: "true",
+      },
+      {
+        arg: "Emp_id",
+        type: "string",
+        required: "true",
+      },
+    ],
+    returns: { arg: "result", type: "any", root: "true" },
+    http: { path: "/seedExistingGame", verb: "post" },
+  });
+
+  // GET ALL GAMES
+  Tbltickets.allGames = function (cb) {
+    dataSource.connector.execute("CALL `getAllGameNamePrice`();", function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      console.log("datum:", data);
+      cb(null, data);
+    });
+  };
+
+  Tbltickets.remoteMethod("allGames", {
+    returns: {
+      arg: "result",
+      type: "any",
+      root: "true",
+    },
+    http: { path: "/allGames", verb: "get" },
   });
 };
